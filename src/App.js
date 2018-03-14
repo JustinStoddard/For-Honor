3681660c734fa11e86f3afcccf43d02399c28b1b
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-14 09:22:45 -0600

    New Files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import CreateForm from './components/CreateForm';
 import Hub from './components/Hub';
 import Welcome from './components/Welcome';
-import { Switch } from 'react-router-dom';
+import Profiles from './components/Profiles';
+import { Switch, Router } from 'react-router-dom';
 
 const App = () => (
   <div className="App">
     <CreateForm />
     <Switch>
-      <Welcome />
-      <Hub />
+      <Router exact path="/" component={Welcome} />
+      <Router exact path="/Hub" component={Hub} />
+      <Router exact path="/Hub/Profiles" component={Profiles} />
     </Switch>
   </div>
 )
